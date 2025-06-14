5d692ebf3fa14f9f1d77f077b9467e2ceb4dab40
Jazz Cometa <jazzcometa11@gmail.com>
2025-06-14 16:27:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-alert(jazz gwapo)
+document.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+          observer.unobserve(entry.target); // only animate once
+        }
+      });
+    }, { threshold: 0.1 });
+
+document.querySelectorAll(".fade-in-on-scroll").forEach(el => observer.observe(el));
+  });
